ad45836be584d1dd7104cf0ce5e9f67f821c736f
Yasmim Roza <yasmimroza16@gmail.com>
2021-11-14 18:38:18 -0400

    Configurado a conexão com o banco.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App{
 
     constructor(){
         this.server = express();
+
+        mongoose.connect('mongodb://localhost:27017/devhouse', {
+            useNewUrlParser: true, useUnifiedTopology: true
+        });
 
         this.middlewares();
         this.routes();
