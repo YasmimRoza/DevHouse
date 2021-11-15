bebe7478c2d23b7b0b4a7c4d5fc8c4bfde8c21c2
Yasmim Roza <yasmimroza16@gmail.com>
2021-11-14 20:41:17 -0400

    Feito a configuração do session(cadastro do usuario), com teste.

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,8 +1,21 @@
 
 //Metodos: index, show, update, store, destroy
+import User from '../models/User';
+
 class SessionController{
-    store(req, res){
-        return res.json({ message: 'Minha Api!'})
+    async store(req, res){
+        const { email } = req.body;
+
+        //Procura o email recebido na requisição
+        let user = await User.findOne({ email });
+
+        //Se não estiver no banco então entre
+        if(!user){
+            //Registra o novo usuario no banco
+            user = await User.create({ email });
+        }
+
+        return res.json(user)
     }
 }
 
