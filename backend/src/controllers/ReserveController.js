680ba03f7a967f3cbf1ae86fdeaa6d1ccf5ab046
Yasmim Roza <yasmimroza16@gmail.com>
2021-11-15 21:17:39 -0400

    Acrescentado no metodo post do reserve, métodos de segurança na pesquisa.

--- backend/src/controllers/ReserveController.js
+++ backend/src/controllers/ReserveController.js
@@ -1,10 +1,38 @@
 import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
 
 class ReserveController{
+
+/**
+ * 
+ *     async index(req, res) {
+        const { user_id } = req.headers;
+
+        const reserves = await Reserve.find({ user: user_id });
+
+        return res.json(reserves);
+    }
+ */
+
     async store(req,res) {
         const { user_id } = req.headers;
         const { house_id } = req.params;
         const { date } = req.body;
+
+        const house = await House.findById(house_id);
+        if(!house){
+            return res.status(400).json({ error: 'Essa casa não existe.'})
+        }
+
+        if(house.status !== true){
+            return res.status(400).json({ error: 'Solicitação indisponível.'})
+        }
+
+        const user = await User.findById(user_id);
+        if(String(user._id) === String(house.user)){
+            return res.status(401).json({ error: 'Reserva não permitida.'})
+        }
 
         const reserve = await Reserve.create({
             user: user_id,
@@ -12,6 +40,7 @@ class ReserveController{
             date,
         });
 
+        //Aqui ele retorna muitas informações para o front
         await reserve.populate('house').populate('user').execPopulate();
 
         return res.json(reserve);
