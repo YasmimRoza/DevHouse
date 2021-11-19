01c7f55cecd8591ce041cbfdb952a4689e3371bb
Yasmim Roza <yasmimroza16@gmail.com>
2021-11-19 12:04:46 -0400

    Feito a validação de email em SessionController, metodo post.

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,10 +1,20 @@
 
 //Metodos: index, show, update, store, destroy
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController{
     async store(req, res){
+
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        })
+
         const { email } = req.body;
+
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({ error: 'Falha na validação.' }); 
+        }
 
         //Procura o email recebido na requisição
         let user = await User.findOne({ email });
